98e4835982ddeacdf1d833469951cb210e6e3dcf
Jiangyifen <jiangyifen@icloud.com>
2014-06-07 21:35:16 +0800

    dd

--- ec/src/T.java
+++ ec/src/T.java
@@ -2,7 +2,7 @@
 public class T {
 
 	public static void main(String args[]) {
-		String s = "123.4567aaabbccc";
+		String s = "123.4567aaabbcccdd";
 		System.out.println(s.substring(0, s.indexOf(".")));
 	}
 
