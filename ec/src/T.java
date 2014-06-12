4bd8a57879ad27acac4cf21412295d9efc5b7116
Bruce Jiang <jiangyifen@gmail.com>
2014-06-12 12:40:51 +0800

    aa

--- ec/src/T.java
+++ ec/src/T.java
@@ -2,7 +2,7 @@
 public class T {
 
 	public static void main(String args[]) {
-		String s = "123.4567aaabbcccdd";
+		String s = "123.4567aaabbcccddee";
 		System.out.println(s.substring(0, s.indexOf(".")));
 	}
 
